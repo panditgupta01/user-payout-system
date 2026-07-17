1510fa2f680b063ef403e9e73e16e979f5cccac9
Amarnath Gupta <amarnathgupta112@gmail.com>
2026-07-17 23:11:17 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("User Payout System API is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
